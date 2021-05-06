4bbd6a5261abb10225b91ead8c7a27517f634fac
Annas <61279556+annasajkh@users.noreply.github.com>
2021-05-06 13:01:01 +0700

    Add files via upload

---
+++ include/pch.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <memory>
+#include <algorithm>
+#include <functional>
+#include <thread>
+#include <utility>
+
+//data structures
+#include <string>
+#include <stack>
+#include <deque>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
